1723fee0618fe09332617afb35e12a9db5911fdf
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-09-06 16:43:31 -0500

    Made progress on implementing visualization

--- src/components/visualization/VisContainer.jsx
+++ src/components/visualization/VisContainer.jsx
@@ -74,13 +74,35 @@ class VisContainer extends Component {
       .attr('fill', '#0cc440')
       .attr('stroke', 'black')
       .attr('stroke-width', 3)
+
+      const prodBrokePipes_group = d3.select('#prod-broke-pipes')
+      .append('g')
+
+      prodBrokePipes_group.selectAll('svg')
+      .data(['service1ToBroker1'])
+      .enter()
+      .append('svg')
+      .attr('width', 300)
+      .attr('height', 300)
+      // .attr('viewBox', '0 0 25 25')
+      .append('line')
+      .attr('x1', 10)
+      .attr('y1', 0)
+      .attr('x2', 150)
+      .attr('y2', 30)
+      // .text(dta => dta)
+      .attr('stroke', 'red')
+      .attr('stroke-width', 15)
+      .attr('opacity', 0.8)
   }
 
   render() {
     return (
       <div id="vis-container">
         <div id="producers"></div>
+        <div id="prod-broke-pipes"></div>
         <div id="brokers"></div>
+        <div id="broke-con-pipes"></div>
         <div id="consumers"></div>
       </div>
     );
@@ -88,3 +110,42 @@ class VisContainer extends Component {
 }
 
 export default VisContainer;
+
+/*
+!!! HTML FOR MAKING NODES !!!
+
+NOTE: IN ORDER FOR ELEMENTS TO PROPERLY OVERLAP, *ALL* SVGs ELEMENTS *MUST* BE IN THE SAVE SVG TAG
+
+<div id="vis-container">
+  <div id="producers">
+    <svg width="300" height="500" id="we1">
+        <circle r="20" cx="50" cy="50" fill="#0cc440" stroke="black" stroke-width="3"></circle>
+        <circle r="20" cx="100" cy="50" fill="#0cc440" stroke="black" stroke-width="3"></circle>
+        <circle r="20" cx="150" cy="50" fill="#0cc440" stroke="black" stroke-width="3"></circle>
+        <line x1="50" y1="70" x2="80" y2="250" stroke="red" stroke-width="8" opacity="0.7"></line>
+      </svg>
+  </div>
+  <div id="prod-broke-pipes">
+    <svg width="300" height="300">
+        <line x1="50" y1="50" x2="" y2="" stroke="red" stroke-width="15" opacity="0.8"></line>
+      </svg>
+  </div>
+  <div id="brokers">
+    <svg width="150" height="150">
+        <rect x="50" y="50" width="50" height="50" rx="10" ry="10" fill="#b02068" stroke="black" stroke-width="3"></rect>
+      </svg><svg width="150" height="150">
+        <rect x="50" y="50" width="50" height="50" rx="10" ry="10" fill="#b02068" stroke="black" stroke-width="3"></rect>
+      </svg>
+  </div>
+  <div id="broke-con-pipes"></div>
+  <div id="consumers">
+    <svg width="100" height="100">
+        <circle r="30" cx="50" cy="50" fill="#0cc440" stroke="black" stroke-width="3"></circle>
+      </svg><svg width="100" height="100">
+        <circle r="30" cx="50" cy="50" fill="#0cc440" stroke="black" stroke-width="3"></circle>
+      </svg><svg width="100" height="100">
+        <circle r="30" cx="50" cy="50" fill="#0cc440" stroke="black" stroke-width="3"></circle>
+      </svg>
+  </div>
+</div>
+*/
