d988eb86fe10632027979f8b26dc16b87bfa5275
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-09-21 16:20:22 -0500

    Adjusted dynamic graphing of plotly chart in DebuggingContainer

--- src/components/debugging-/DebuggingContainer.jsx
+++ src/components/debugging-/DebuggingContainer.jsx
@@ -9,15 +9,87 @@ class DebuggingContainer extends Component {
     super(props)
 
     this.state = {
+      currMetric: 'kafka_server_brokertopicmetrics_bytesin_total', //kafka_controller_kafkacontroller_globaltopiccount
       xData: [],
+      yData: [],
+      startTime: (Date.now() / 1000) - 3600,
+      endTime: Date.now() / 1000,
+      step: 25,
+      fetchedLatest: false,
+    }
+
+    this.fetchGraph = this.fetchGraph.bind(this);
+  }
+
+  fetchGraph() {
+    try {
+      // fetch('http://' + `${this.props.prometheusAddress}` + '/api/v1/query_range?query=kafka_controller_kafkacontroller_globaltopiccount&start=1632244155.671&end=1632251355.671&step=28')
+      fetch(
+        'http://' + `${this.props.prometheusAddress}` + 
+        '/api/v1/query_range?query=' + this.state.currMetric + 
+        '&start=' + `${this.state.startTime}` + 
+        '&end=' + `${this.state.endTime}` + 
+        '&step=' + `${this.state.step}`
+      )
+      .then(res => res.json())
+      .then(res => res['data']['result'][0]['values'])
+      .then(array => {
+        console.log(array);
+
+        let newX = [];
+        let newY = [];
+
+        array.forEach(arr => {
+          newX.push(Number(arr[0]))
+          newY.push(Number(arr[1]))
+        });
+
+        console.log(newX);
+        console.log(newY);
+
+        this.setState({
+          xData: newX,
+          yData: newY,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        console.log('Did you put in a proper URL for the Prometheus address?');
+      });
+    } catch (e) {
+      console.log(e); 
     }
   }
 
+  xBoundsIncrease() {
+    console.log('executing xBoundsIncrease');
+
+    let newStartTime = this.state.startTime - 3600;
+
+    console.log(newStartTime);
+
+    this.setState({
+      startTime: newStartTime,
+      fetchedLatest: false,
+    });
+  }
+
   componentDidMount() {
-    try {
-      fetch(prometheusAddress + '')
-    } catch (e) {
-      console.log(e);
+    if (!this.state.fetchedLatest) {
+      this.fetchGraph();
+      this.setState({
+        fetchedLatest: true,
+      });
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('We updated!');
+    if (!this.state.fetchedLatest) {
+      this.fetchGraph();
+      this.setState({
+        fetchedLatest: true,
+      });
     }
   }
 
@@ -28,8 +100,8 @@ class DebuggingContainer extends Component {
         <Plot
           data ={[
             {
-              x: [1, 2, 3],
-              y: [1, 8, 27],
+              x: this.state.xData,
+              y: this.state.yData,
               type: 'bar',
             },
           ]}
@@ -37,11 +109,13 @@ class DebuggingContainer extends Component {
           layout = {{
             width: 800,
             height: 600,
-            title: 'x^3',
+            title: this.state.currMetric,
           }}
         />
 
         <p>Prometheus address currently entered: {this.props.prometheusAddress}</p>
+
+        <button className="button is-primary is-light" onClick={() => {this.xBoundsIncrease()}}>Back 1 hr</button>
       </div>
     );
   };
