5f4ecc626c3966c00a23e8001058ac5699a692ba
George <iSaySureWhyNot@gmail.com>
2021-09-21 11:38:31 -0400

    rewrote return html sections

--- src/components/other/DropdownMenu.jsx
+++ src/components/other/DropdownMenu.jsx
@@ -28,3 +28,27 @@ const DropdownMenu = () => {
         }
       
       }, [isActive]);
+      
+      return (
+        <div className="menu-container">
+          <button onClick={onClick} className="menu-trigger">
+            <span>User</span>
+            <img src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/df/df7789f313571604c0e4fb82154f7ee93d9989c6.jpg" alt="User avatar" />
+          </button>
+          <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
+            <ul>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+              <li><button onClick={onClickQuery} className="menu-trigger" value="kafka_cluster_partition_underreplicated">kafka_cluster_partition_underreplicated</button></li>
+            </ul>
+          </nav>
+        </div>
+      );
+    };
