5061b8b6d6909b6d57ae80df2759caeb26c9387c
George <iSaySureWhyNot@gmail.com>
2021-09-21 11:37:54 -0400

    rewrote useEffect

--- src/components/other/DropdownMenu.jsx
+++ src/components/other/DropdownMenu.jsx
@@ -10,3 +10,21 @@ const DropdownMenu = () => {
         console.log(e.target.value)
         setQuery(e.target.value);
     }
+    useEffect(() => {
+        const pageClickEvent = (e) => {
+            // If the active element exists and is clicked outside of
+            if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+              setIsActive(!isActive);
+            }
+          };
+      
+        // If the item is active (ie open) then listen for clicks
+        if (isActive) {
+          window.addEventListener('click', pageClickEvent);
+        }
+      
+        return () => {
+          window.removeEventListener('click', pageClickEvent);
+        }
+      
+      }, [isActive]);
