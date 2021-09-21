394de05f44549ff35704b35d364b5dfd377887e9
George <iSaySureWhyNot@gmail.com>
2021-09-21 13:30:22 -0400

    updated text of dropdown menu

--- src/components/other/DropdownMenu.jsx
+++ src/components/other/DropdownMenu.jsx
@@ -32,7 +32,7 @@ const DropdownMenu = () => {
       return (
         <div className="menu-container">
           <button onClick={onClick} className="menu-trigger">
-            <span>User</span>
+            <span>Query</span>
             <img src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/df/df7789f313571604c0e4fb82154f7ee93d9989c6.jpg" alt="User avatar" />
           </button>
           <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
