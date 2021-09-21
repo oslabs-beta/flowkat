4ee851bd9a6c58e9246d6543b951966b4b85348c
George <iSaySureWhyNot@gmail.com>
2021-09-21 11:36:17 -0400

    initial function

--- src/components/other/DropdownMenu.jsx
+++ src/components/other/DropdownMenu.jsx
@@ -1,2 +1,12 @@
 import React, { Component, useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+
+const DropdownMenu = () => {
+    const dropdownRef = useRef(null);
+    const [isActive, setIsActive] = useState(false);
+    const [query, setQuery] = useState('');
+    const onClick = () => setIsActive(!isActive);
+    const onClickQuery = (e) =>{
+        console.log(e.target.value)
+        setQuery(e.target.value);
+    }
