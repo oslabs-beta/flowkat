85d38a9d1436203344d2ca65d26cf4bcb3b5a886
Miguel Hernandez <miguelh72@outlook.com>
2021-10-10 13:08:48 -0400

    converted MetricsContainer to functional component
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- src/components/metrics/MetricsContainer.jsx
+++ src/components/metrics/MetricsContainer.jsx
@@ -1,123 +1,100 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Plot from 'react-plotly.js';
-import DropdownMenu from '../other/DropdownMenu.jsx';
+import DropdownMenu from './DropdownMenu.jsx';
 
-class MetricsContainer extends Component {
-  constructor(props) {
-    super(props)
+export default function MetricsContainer({
+  prometheusAddress,
+}) {
+  const [state, setState] = useState({
+    currMetric: 'kafka_server_brokertopicmetrics_bytesin_total',
+    fetchedLatest: false,
+    xData: [],
+    yData: [],
+    startTime: (Date.now() / 1000) - 3600,
+    endTime: Date.now() / 1000,
+    step: 20,
+    queryString: '',
+  });
 
-    this.state = {
-      currMetric: 'kafka_server_brokertopicmetrics_bytesin_total',
-      xData: [],
-      yData: [],
-      startTime: (Date.now() / 1000) - 3600,
-      endTime: Date.now() / 1000,
-      step: 20,
+  const onClickQuery = useCallback(function onClickQuery(currMetric) {
+    setState(state => ({
+      ...state,
+      currMetric,
       fetchedLatest: false,
-      queryString: '',
-    }
+    }));
+  }, []);
 
-    this.fetchGraph = this.fetchGraph.bind(this);
-    this.onClickQuery = this.onClickQuery.bind(this);
-  }
+  const fetchGraph = useCallback(function fetchGraph() {
+    const fetchURL = `http://${prometheusAddress}/api/v1/query_range?query=${state.currMetric}&start=${state.startTime}&end=${state.endTime}&step=${state.step}`;
 
-  onClickQuery(event){
-    this.setState({
-      currMetric: event.target.value,
-      fetchedLatest: false,
-    });
-  }
-
-  fetchGraph() {
-    let fetchURL = 'http://' + `${this.props.prometheusAddress}` + '/api/v1/query_range?query=' + this.state.currMetric + '&start=' + `${this.state.startTime}` + '&end=' + `${this.state.endTime}` + '&step=' + `${this.state.step}`;
-
-    try {
-      fetch(fetchURL)
+    fetch(fetchURL)
       .then(res => res.json())
-      .then(res => res['data']['result'][0]['values'])
-      .then(array => {
-        let newX = [];
-        let newY = [];
+      .then(res => {
+        const array = res['data']['result'][0]['values'];
+        const latestTimeInSec = array[array.length - 1][0];
 
-        let latestTimeInSec = array[array.length - 1][0]
-
+        const newX = [], newY = [];
         array.forEach(arr => {
-          newX.push( (Number(arr[0]) - latestTimeInSec) / 3600);
+          newX.push((Number(arr[0]) - latestTimeInSec) / 3600);
           newY.push(Number(arr[1]));
         });
 
-        this.setState({
+        setState(state => ({
+          ...state,
+          fetchedLatest: true,
           xData: newX,
           yData: newY,
-        });
+        }));
       })
       .catch(err => {
-        console.log(err);
+        console.error(err);
         console.log('Did you put in a proper URL for the Prometheus address?');
       });
-    } catch (e) {
-      console.log(e); 
-    }
-  }
+  }, [prometheusAddress, state.currMetric, state.endTime, state.startTime, state.step]);
 
-  xBoundsIncrease() {
-    let newStartTime = this.state.startTime - 3600;
+  const xBoundsIncrease = useCallback(function xBoundsIncrease() {
+    const newStartTime = state.startTime - 3600;
 
-    this.setState({
+    this.setState(state => ({
+      ...state,
       startTime: newStartTime,
       fetchedLatest: false,
-    });
-  }
+    }));
+  }, [state.startTime]);
 
-  componentDidMount() {
-    if (!this.state.fetchedLatest) {
-      this.fetchGraph();
-      this.setState({
-        fetchedLatest: true,
-      });
+  useEffect(() => {
+    if (!state.fetchedLatest) {
+      fetchGraph();
     }
-  }
+  }, [fetchGraph, state.fetchedLatest]);
 
-  componentDidUpdate() {
-    if (!this.state.fetchedLatest) {
-      this.fetchGraph();
-      this.setState({
-        fetchedLatest: true,
-      });
-    }
-  }
+  return (
+    <div id="metrics-container">
+      <DropdownMenu
+        prometheusAddress={prometheusAddress}
+        onClickQuery={onClickQuery}
+      />
+      <Plot
+        data={[
+          {
+            x: state.xData,
+            y: state.yData,
+            type: 'bar',
+          },
+        ]}
 
-  render() {
-    return (
-      <div id="metrics-container">
-        <DropdownMenu 
-          prometheusAddress={this.props.prometheusAddress}
-          onClickQuery={this.onClickQuery}
-          />
-        <Plot
-          data ={[
-            {
-              x: this.state.xData,
-              y: this.state.yData,
-              type: 'bar',
-            },
-          ]}
+        layout={{
+          width: 800,
+          height: 600,
+          title: state.currMetric,
+          plot_bgcolor: "#fffaeb",
+          paper_bgcolor: "#fffaeb",
+        }}
+      />
 
-          layout = {{
-            width: 800,
-            height: 600,
-            title: this.state.currMetric,
-            plot_bgcolor: "#fffaeb",
-            paper_bgcolor: "#fffaeb",
-          }}
-        />
+      <p>Prometheus address currently entered: {prometheusAddress}</p>
 
-        <p>Prometheus address currently entered: {this.props.prometheusAddress}</p>
-
-        <button className="button is-primary is-light" onClick={() => {this.xBoundsIncrease()}}>Back 1 hr</button>
-      </div>
-    );
-  };
+      <button className="button is-primary is-light" onClick={xBoundsIncrease}>Back 1 hr</button>
+    </div>
+  );
 }
-
-export default MetricsContainer;
