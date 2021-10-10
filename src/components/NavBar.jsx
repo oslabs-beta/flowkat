005e09242e637f5aa232867389a34e598ab618bd
Miguel Hernandez <miguelh72@outlook.com>
2021-10-10 13:10:36 -0400

    Converted NavBar to functional component
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 // NavBar anchored to left of app, for changing to different views using react router
-class NavBar extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div id="nav-bar">
-        <Link to="/" className="button is-primary">Main</Link>
-        <Link to="/messages" className="button is-primary">Messages</Link>
-        <Link to="/metrics" className="button is-primary">Metrics</Link>
-        <Link to="/options" className="button is-primary">Options</Link>
-      </div>
-    );
-  };
+export default function NavBar() {
+  return (
+    <div id="nav-bar">
+      <Link to="/" className="button is-primary">Main</Link>
+      <Link to="/messages" className="button is-primary">Messages</Link>
+      <Link to="/metrics" className="button is-primary">Metrics</Link>
+      <Link to="/options" className="button is-primary">Options</Link>
+    </div>
+  );
 }
-
-export default NavBar;
